661a93344185831c042a41fd5576ac662c9c89ac
Alex Rogers <alex.rogers823@gmail.com>
2020-09-13 15:09:05 -0400

    created member component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Member from './Member';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    name: "Oscar Rogers",
+    birth: "January 1, 1910",
+    death: "April 5, 1994",
+    birthplace: "Iuka, MS",
+    residence: "Gary, IN",
+    parents: ["Mark Rogers", "Gene Rogers"],
+    children: ["Patricia Rogers", "Rogercarole Rogers", "Oscar Barry Rogers"],
+    spouse: "Helen French Rogers"
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Member 
+          name={this.state.name} 
+          birth={this.state.birth}
+          death={this.state.death}
+          birthplace={this.state.birthplace}
+          residence={this.state.residence}
+          parents={this.state.parents}
+          offspring={this.state.children}
+          spouse={this.state.spouse}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
