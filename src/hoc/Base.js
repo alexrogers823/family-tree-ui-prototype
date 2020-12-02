ccec3e958c9088dc4c32d966c69800715e9bb13b
Alex Rogers <alex.rogers823@gmail.com>
2020-12-02 17:34:11 -0500

    added universal background color to Base component

--- src/hoc/Base.js
+++ src/hoc/Base.js
@@ -1,14 +1,24 @@
 import React, { Fragment } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: 'rgba(47, 115, 169, 0.3)',
+  }
+});
+
 const Base = props => {
+  const classes = useStyles();
+
   return (
-    <Fragment>
+    <div className={classes.root}>
       <Header />
       {props.children}
       <Footer />
-    </Fragment>
+    </div>
   )
 }
 
