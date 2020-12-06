680b7a0e0fb468695511de8422915dc59dcb0378
Alex Rogers <alex.rogers823@gmail.com>
2020-12-06 17:01:08 -0500

    added event modal to timeline event page

--- src/components/AddEventModal/index.js
+++ src/components/AddEventModal/index.js
@@ -4,15 +4,11 @@ import Form, { TextArea } from '../common/Form';
 
 const AddEventModal = props => {
   return (
-    // <Modal>
-    //   <h1>Add an event</h1>
-      // <input type="text" placeholder="Date (MM/DD/YYYY)" />
-      // <textarea placeholder="Event" />
-    //   <Button>POST IT!</Button>
-    // </Modal>
     <Form 
       title="Add an event"
       button="POST IT!"
+      isOpen={props.isOpen}
+      closeModal={props.closeModal}
     >
       <Date label="Event Date" />
       <TextArea 
--- src/modules/TimelineEvents/index.js
+++ src/modules/TimelineEvents/index.js
@@ -1,20 +1,43 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+
+import AddEventModal from '../../components/AddEventModal';
+import Button from '../../components/common/Button';
 import Timeline from './Timeline';
 
+
+const useStyles = makeStyles(theme => ({
+  title: {
+    fontWeight: 'bold',
+  },
+  topLevel: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: theme.spacing(2)
+  }
+}));
+
 const TimelineEvents = props => {
+  const classes = useStyles();
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <Fragment>
-      <h1>Timeline of Family Events</h1>
+      <div className={classes.topLevel}>
+        <h1>Timeline of Family Events</h1>
+        <Button onClick={() => setOpenModal(true)}>Add an event</Button>
+      </div>
       {props.timelineEvents.map(ev => {
         return (
           <Timeline 
-            year={ev.year} 
-            timelineEvent={ev.timelineEvent} 
-            key={`event_${ev.id}`} 
+          year={ev.year} 
+          timelineEvent={ev.timelineEvent} 
+          key={`event_${ev.id}`} 
           />
           )
         })}
+      <AddEventModal isOpen={openModal} closeModal={() => setOpenModal(false)} />
     </Fragment>
   )
 };
