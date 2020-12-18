d33f28915b939bd6f8ec081807d708464346169c
Alex Rogers <alex.rogers823@gmail.com>
2020-12-18 17:19:50 -0600

    added Tabs component inside settings for easier navigation

--- src/components/UserSettings/index.js
+++ src/components/UserSettings/index.js
@@ -1,16 +1,109 @@
-import React, { Fragment } from 'react';
-import Button from '../common/Button';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+import memberInformationSettings from './memberInformationSettings';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  console.log('chidren', children);
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `user-settings-tab-${index}`,
+    'aria-controls': `user-settings-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: 224,
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
 
 const UserSettings = props => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <Fragment>
-      <h1>User Settings Page</h1>
-      <p>Username/Member Name: {props.firstName} {props.lastName}</p>
-      <p>Email: {props.email}</p>
-      <p>Last logged in: October 24</p>
-      <Button>Change Password</Button>
-    </Fragment>
-  )
-};
+    <div className={classes.root}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="User Settings Tabs"
+        className={classes.tabs}
+      >
+        <Tab label="Member Information" {...a11yProps(0)} />
+        <Tab label="Item Two" {...a11yProps(1)} />
+        <Tab label="Item Three" {...a11yProps(2)} />
+        <Tab label="Item Four" {...a11yProps(3)} />
+        <Tab label="Item Five" {...a11yProps(4)} />
+        <Tab label="Item Six" {...a11yProps(5)} />
+        <Tab label="Item Seven" {...a11yProps(6)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <memberInformationSettings />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Test
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Test
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Test
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Test
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+        Test
+      </TabPanel>
+      <TabPanel value={value} index={6}>
+        Test
+      </TabPanel>
+    </div>
+  );
+}
 
 export default UserSettings;
---
+++ src/components/UserSettings/memberInformationSettings.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import Button from '../../components/common/Button';
+
+const memberInformationSettings = props => {
+  return (
+    <p>Test inside function</p>
+  )
+};
+
+{/* <Fragment>
+<p>Username/Member Name: {props.firstName} {props.lastName}</p>
+<p>Email: {props.email}</p>
+<p>Last logged in: October 24</p>
+<Button>Change Password</Button>
+</Fragment> */}
+
+export default memberInformationSettings;
