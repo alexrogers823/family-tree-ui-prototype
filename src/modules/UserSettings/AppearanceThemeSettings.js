40caa96f7397aafffe463c58e736157306cf1cf3
Alex Rogers <alex.rogers823@gmail.com>
2020-12-22 14:37:01 -0600

    added radio buttons for appearence settings fragment

--- src/modules/UserSettings/AppearanceThemeSettings.js
+++ src/modules/UserSettings/AppearanceThemeSettings.js
@@ -1,12 +1,31 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import {
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel
+} from '@material-ui/core';
+
 import Button from '../../components/common/Button';
 
 const AppearanceThemeSettings = props => {
+  const [value, setValue] = useState('light');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  }
+
   return (
     <Fragment>
-      <p>Light Theme</p>
-      <p>Dark Theme</p>
-      <Button>Save</Button>
+      <FormControl component="fieldset">
+        <FormLabel component="legend">Appearance Theme</FormLabel>
+        <RadioGroup aria-label="theme" name="color-theme" value={value} onChange={handleChange}>
+          <FormControlLabel value="light" control={<Radio />} label="Light Theme" />
+          <FormControlLabel value="dark" control={<Radio />} label="Dark Theme" />
+        </RadioGroup>
+        <Button>Save</Button>
+      </FormControl>
     </Fragment>
   )
 };
