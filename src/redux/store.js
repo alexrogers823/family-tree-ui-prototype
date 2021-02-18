df1c3780eee80d24cb808c2a42ee21a555c2815b
Alex Rogers <alex.rogers823@gmail.com>
2021-02-17 22:00:52 -0500

    reimplemented redux I

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,8 @@ import { createStore } from 'redux';
 import reducer from './reducers';
 
 const store = createStore(reducer);
-// store.dispatch({});
-
+console.log('Initial state', store.getState());
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
+// store.dispatch()
+unsubscribe();
 export default store;
