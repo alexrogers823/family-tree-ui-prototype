7e109807e65e59a7a5099c7e00a2dc4387e85c89
Alex Rogers <alex.rogers823@gmail.com>
2021-02-27 21:49:53 -0500

    added action getArtifactById to redux

--- src/modules/Artifacts/redux/actions.js
+++ src/modules/Artifacts/redux/actions.js
@@ -1,9 +1,17 @@
 import {
-  GET_ALL_ARTIFACTS
+  GET_ALL_ARTIFACTS,
+  GET_ARTIFACT_BY_ID,
 } from './actionTypes';
 
 export const getAllArtifacts = () => {
   return {
     type: GET_ALL_ARTIFACTS
+  }
+}
+
+export const getArtifactById = (id = 0) => {
+  return {
+    type: GET_ARTIFACT_BY_ID,
+    payload: id
   }
 }
--- src/modules/Artifacts/redux/reducer.js
+++ src/modules/Artifacts/redux/reducer.js
@@ -1,11 +1,13 @@
 import {
-  GET_ALL_ARTIFACTS
+  GET_ALL_ARTIFACTS,
+  GET_ARTIFACT_BY_ID
 } from './actionTypes';
 
 import { artifactData } from '../../../dummyData';
 
 const INIT_STATE = {
-  artifacts: []
+  artifacts: [],
+  artfact: {}
 };
 
 export default (state = INIT_STATE, action) => {
@@ -14,6 +16,12 @@ export default (state = INIT_STATE, action) => {
       return {
         ...state,
         artifacts: [ ...artifactData ]
+      }
+
+    case GET_ARTIFACT_BY_ID:
+      return {
+        ...state,
+        artifact: artifacts.find(doc => doc.id === action.payload)
       }
 
     default:
