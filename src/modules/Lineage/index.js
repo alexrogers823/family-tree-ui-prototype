ec446db4759808403d13adc82ff509331d765f64
Alex Rogers <alex.rogers823@gmail.com>
2023-02-24 21:53:08 -0500

    replaced dummy data with actual tree data in lineage

--- src/modules/Lineage/index.js
+++ src/modules/Lineage/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useLayoutEffect, useState } from 'react';
+import React, { useRef, useEffect, useLayoutEffect, useState, useMemo, useCallback } from 'react';
 import MemberCard from '../../components/MemberCard';
 import Link from '../../components/common/Link';
 import * as d3 from 'd3';
@@ -15,7 +15,7 @@ const Lineage = props => {
   const svgMargin = 50;
   const width  = window.innerWidth;
   const height = window.innerHeight;
-  const margin = {left: 20, right: 20};
+  const margin = {left: 20, right: 20, top: 20};
 
   const nameFontSize = 10;
 
@@ -42,222 +42,21 @@ const Lineage = props => {
     } else if (type === "leaf") {
       return `M ${memberData.target.x + margin.right}, ${memberData.target.y} h 40`;
     }
-    return `M ${memberData.source.x - margin.left}, ${memberData.source.y} h 60 v ${memberCardLength} H ${memberData.target.x - margin.left} V ${memberData.target.y}`;
+    return `M ${memberData.source.x - margin.left}, ${memberData.source.y} h 60 v ${memberCardLength - margin.top} H ${memberData.target.x - margin.left} V ${memberData.target.y}`;
   }
   // ===
 
-  const fetchData = async() => {
-    const response = await fetch("/api/members/tree/")
-    if (!response.ok) {
-      throw new Error("Error fetching data");
-    }
-    return response.json();
-  }
-
-  console.log(lineageData);
-
   useLayoutEffect(() => { // useLayoutEffect for synchronous code to avoid issues with DOM
-    // fetchData()
-    //   .then(response => setLineageData(response))
-    //   .catch(error => {
-    //     console.error(error)
-    //   });
-
-    d3.json("/api/members/tree/")
+      
+    d3.json("/api/members/tree/", d => console.log(d))
       .then(data => {
         setLineageData(data);
         setLoading(false);
       });
 
-    const sampleData = {
-      id: 3,
-      preferredName: "Helen",
-      lastName: "Rogers",
-      isInLaw: false,
-      birthYear: 1920,
-      deathYear: 2021,
-      spouseId: 7,
-      spouse: {
-        id: 7,
-        preferredName: "Oscar",
-        lastName: "Rogers",
-        isInLaw: true,
-        spouseId: 3,
-        birthYear: 1915,
-        deathYear: 1994,
-        primaryParentId: null,
-        secondaryParentId: null 
-      },
-      children: [
-        {
-          id: 8,
-          preferredName: "Patricia",
-          lastName: "Crozier",
-          isInLaw: false,
-          spouseId: 9,
-          children: [
-            {
-              id: 12,
-              preferredName: "Angie",
-              lastName: "Peasant",
-              isInLaw: false,
-              spouseId: 13,
-              children: null
-            },
-            {
-              id: 14,
-              preferredName: "Adella",
-              lastName: "Crozier",
-              isInLaw: false,
-              spouseId: 15,
-              children: null
-            },
-            {
-              id: 21,
-              preferredName: "Ronnie",
-              lastName: "Crozier",
-              isInLaw: false,
-              spouseId: 16,
-              children: null
-            }
-          ]
-        },
-        {
-          id: 10,
-          preferredName: "Rogercarole",
-          lastName: "Rogers-Hall",
-          isInLaw: false,
-          spouseId: 11,
-          children: [
-            {
-              id: 6,
-              preferredName: "Simone",
-              lastName: "Banks",
-              isInLaw: false,
-              spouseId: 17,
-              children: null
-            }
-          ]
-        },
-        {
-          id: 4,
-          preferredName: "Barry",
-          lastName: "Rogers",
-          isInLaw: false,
-          spouseId: null,
-          children: [
-            {
-              id: 5,
-              preferredName: "Alex",
-              lastName: "Rogers",
-              isInLaw: false,
-              spouseId: null,
-              children: null
-            }
-          ]
-        }
-      ]
-    };
-
-    const sampleData2 = [
-      {
-        id: 3,
-        preferredName: "Helen",
-        lastName: "Rogers",
-        isInLaw: false,
-        spouseId: 7,
-        birthYear: 1920,
-        deathYear: 2021,
-        primaryParentId: null,
-        secondaryParentId: null
-      },
-      {
-        id: 7,
-        preferredName: "Oscar",
-        lastName: "Rogers",
-        isInLaw: true,
-        spouseId: 3,
-        birthYear: 1915,
-        deathYear: 1994,
-        primaryParentId: null,
-        secondaryParentId: null 
-      },
-      {
-        id: 8,
-        preferredName: "Patricia",
-        lastName: "Crozier",
-        isInLaw: false,
-        spouseId: 9,
-        birthYear: 1947,
-        primaryParentId: 3,
-        secondaryParentId: 7
-      },
-      {
-        id: 12,
-        preferredName: "Angie",
-        lastName: "Peasant",
-        isInLaw: false,
-        spouseId: 13,
-        primaryParentId: 8,
-        secondaryParentId: 9
-      },
-      {
-        id: 14,
-        preferredName: "Adella",
-        lastName: "Crozier",
-        isInLaw: false,
-        spouseId: 15,
-        primaryParentId: 8,
-        secondaryParentId: 9
-      },
-      {
-        id: 21,
-        preferredName: "Ronnie",
-        lastName: "Crozier",
-        isInLaw: false,
-        spouseId: 16,
-        primaryParentId: 8,
-        secondaryParentId: 9
-      },
-      {
-        id: 10,
-        preferredName: "Rogercarole",
-        lastName: "Rogers-Hall",
-        isInLaw: false,
-        spouseId: 11,
-        primaryParentId: 3,
-        secondaryParentId: 7
-      },
-      {
-        id: 6,
-        preferredName: "Simone",
-        lastName: "Banks",
-        isInLaw: false,
-        spouseId: 17,
-        primaryParentId: 10,
-        secondaryParentId: 11
-      },
-      {
-        id: 4,
-        preferredName: "Barry",
-        lastName: "Rogers",
-        isInLaw: false,
-        spouseId: null,
-        primaryParentId: 3,
-        secondaryParentId: 7
-      },
-      {
-        id: 5,
-        preferredName: "Alex",
-        lastName: "Rogers",
-        isInLaw: false,
-        spouseId: null,
-        primaryParentId: 4,
-        secondaryParentId: null
-      }
-    ]
-
     // initializing the svg 
+    d3.select(treeRef.current).selectAll("svg").remove(); // Removes old content upon re-render, including initial value from lineageData
+
     const svg = d3.select(treeRef.current)
                   .append("svg")
                   .attr("width", width)
@@ -270,8 +69,9 @@ const Lineage = props => {
     //                 .on("zoom", () => svg.attr("transform", d3.event.transform))
   
     // arranging the data into d3 
-    // const treeStructure = d3.hierarchy(lineageData);
-    const treeStructure = d3.hierarchy(sampleData);
+    const treeStructure = d3.hierarchy(lineageData);
+    console.log('data inside', treeStructure);
+    // const treeStructure = d3.hierarchy(sampleData);
     // const treeStructure = d3.stratify()
     //                         .id(d => d.id)
     //                         .parentId(d => d.primaryParentId)
@@ -284,43 +84,48 @@ const Lineage = props => {
 
     // making the data from the tree accessible 
     const treeLineage = treeLayout(treeStructure);
-    console.log('tree info: ', treeLineage.links());
+    console.log('tree info: ', treeLineage.descendants());
 
     // setting the connecting lines 
     const connector = svg.append("g")
                           .selectAll("path")
-                          .data(treeLineage.links());
+                          .data(treeLineage.links(), d => d);
 
     // connecting line for primary parent 
     connector.enter()
                 .append("path")
+                .merge(connector)
                 .attr("d", d => createConnector(d))
                 .style("fill", "none")
                 .style("stroke", "silver")
-                .style("stroke-width", "2");
+                .style("stroke-width", "2")
 
     // connecting line for secondary parent 
     connector.enter()
                 .append("path")
+                .merge(connector)
                 .attr("d", d => createConnector(d, "secondary"))
-                .style("display", d => d.source.data.spouseId ? "inherit" : "none")
+                .style("display", d => d.source.data.spouse ? "inherit" : "none")
                 .style("fill", "none")
                 .style("stroke", "silver")
-                .style("stroke-width", "2");
+                .style("stroke-width", "2")
 
     // connecting line for leaf (if married)
     connector.enter()
               .append("path")
+              .merge(connector)
               .attr("d", d => createConnector(d, "leaf"))
-              .style("display", d => d.target.data.spouseId ? "inherit" : "none")
+              .style("display", d => d.target.data.spouse ? "inherit" : "none")
               .style("fill", "none")
               .style("stroke", "silver")
-              .style("stroke-width", "2");
+              .style("stroke-width", "2")
+
+    // connector.exit().remove();
 
     // setting the area for the primary parent 
     const primaryMemberCard = svg.append("g")
                           .selectAll("rect")
-                          .data(treeLineage.descendants());
+                          .data(treeLineage.descendants(), d => d);
 
     primaryMemberCard.enter()
               .append("a")
@@ -332,28 +137,32 @@ const Lineage = props => {
               .style("stroke", "silver")
               .style("width", memberCardLength)
               .style("height", memberCardLength)
-              .style("stroke-width", "2px");
+              .style("stroke-width", "2px")
+
+    // primaryMemberCard.exit().remove();
 
     // setting the area for the secondary parent 
     const secondaryMemberCard = svg.append("g")
                                 .selectAll("rect")
-                                .data(treeLineage.descendants());
+                                .data(treeLineage.descendants(), d => d);
 
     secondaryMemberCard.enter()
+                    .append("a")
+                    .attr("xlink:href", d => d.data.spouse ? `${memberUrl}/${d.data.spouse.id}` : null )
                     .append("rect")
                     .attr("x", d => d.x + (memberCardLength / 2) + margin.right)
                     .attr("y", d => d.y - (memberCardLength / 2))
-                    .style("display", d => d.data.spouseId ? "inherit" : "none")
+                    .style("display", d => d.data.spouse ? "inherit" : "none")
                     .style("fill", "white")
                     .style("stroke", "silver")
                     .style("width", memberCardLength)
                     .style("height", memberCardLength)
-                    .style("stroke-width", "2px");
+                    .style("stroke-width", "2px")
 
     // creating text for the primary parent 
     const primaryMemberInfo = svg.append("g")
                       .selectAll("text")
-                      .data(treeLineage.descendants());
+                      .data(treeLineage.descendants(), d => d);
 
     primaryMemberInfo.enter()
             .append("text")
@@ -362,8 +171,7 @@ const Lineage = props => {
             .attr("y", d => d.y + memberCardNameLength) //may change later
             .style("dominant-baseline", "middle")
             .style("text-anchor", "middle")
-            .style("font-size", nameFontSize);
-
+            .style("font-size", nameFontSize)
 
     primaryMemberInfo.enter()
             .append("text")
@@ -372,37 +180,34 @@ const Lineage = props => {
             .attr("y", d => d.y + memberCardTimeLength)
             .style("dominant-baseline", "middle")
             .style("text-anchor", "middle")
-            .style("font-size", memberCardFontSize);
-
-    // const contentLink = content.append("g").selectAll(linkRef.current).data(treeLineage.descendants());
-
-    // contentLink.enter().append(linkRef.current).attr("href", "localhost:3000");
+            .style("font-size", memberCardFontSize)
 
     // creating text for the secondary parent 
     const secondaryMemberInfo = svg.append("g")
                           .selectAll("text")
-                          .data(treeLineage.descendants())
+                          .data(treeLineage.descendants(), d => d);
 
     secondaryMemberInfo.enter()
               .append("text")
-              .text(d => d.data.spouse ? `${d.data.spouse.preferredName} ${d.data.spouse.lastName}` : "test")
+              .text(d => d.data.spouse ? `${d.data.spouse.preferredName} ${d.data.spouse.lastName}` : null)
               .attr("x", d => d.x + 100)
               .attr("y", d => d.y + memberCardNameLength) //may change later
-              .style("display", d => d.data.spouseId ? "inherit" : "none")
+              .style("display", d => d.data.spouse ? "inherit" : "none")
               .style("dominant-baseline", "middle")
               .style("text-anchor", "middle")
-              .style("font-size", nameFontSize);
-
+              .style("font-size", nameFontSize)
+              
     secondaryMemberInfo.enter()
               .append("text")
               .text(d => d.data.spouse ? arrangeMemberTimeline(d.data.spouse) : null) //Birth Year and Death Year
               .attr("x", d => d.x + 100)
               .attr("y", d => d.y + memberCardTimeLength)
+              .style("display", d => d.data.spouse ? "inherit" : "none")
               .style("dominant-baseline", "middle")
               .style("text-anchor", "middle")
-              .style("font-size", memberCardFontSize);
+              .style("font-size", memberCardFontSize)
 
-  }, []);
+  }, [JSON.stringify(lineageData)]);
 
 
   // return (
