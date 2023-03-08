16a183ad462613c79afef131c72405f10875dd70
Alex Rogers <alex.rogers823@gmail.com>
2023-03-08 18:21:14 -0500

    added basic styling for members page

--- src/modules/Members/index.js
+++ src/modules/Members/index.js
@@ -1,20 +1,29 @@
-import React, { Fragment, useEffect } from 'react';
+import { Box, Grid, Paper, styled } from '@mui/material';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  Route, Switch, useRouteMatch
-} from "react-router-dom";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
 import Link from '../../components/common/Link';
 import Member from './Member';
 import { getAllFamilyMembers } from './redux/actions';
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'left',
+  color: theme.palette.text.secondary,
+}));
+
 const arrangeMemberLink = (member, url) => {
   return (
-    <>
-      <Link href={`${url}/${member.id}`}>
-        {member.preferredName || member.firstName} {member.lastName}
-      </Link>
+    <Grid xs={3} spacing={2}>
+      <Item elevation={3}>
+        <Link href={`${url}/${member.id}`}>
+          {member.preferredName || member.firstName} {member.lastName}
+        </Link>
+      </Item>
       <br />
-    </>
+    </Grid>
   )
 };
 
@@ -40,16 +49,16 @@ const Members = () => {
           <Member />
         </Route>
         <Route path={match.path}>
-          <Fragment>
-            <h3>Family member pages by their id. Put '/1' after the url as an example.</h3>
-            <h5>List of existing members</h5>
-            {familyMembers.map(person => arrangeMemberLink(person, match.path))}
-          </Fragment>
+          <Box sx={{ width: '95%', margin: 2 }}>
+            <h3>List of existing members</h3>
+            <Grid container rowSpacing={2} columnSpacing={2}>
+              {familyMembers.map(person => arrangeMemberLink(person, match.path))}
+            </Grid>
+          </Box>
         </Route>
       </Switch>
     </div>
   )
-  
 };
 
 export default Members;
